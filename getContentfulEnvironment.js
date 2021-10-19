7dd253e33c0dea9eb2a26f829eb6ff679852029c
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-19 18:49:48 -0500

    Move `contentful-management` to dev dependency and install `contentful`

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -1,6 +1,9 @@
 require('dotenv').config({ path: '.env.local' });
 const contentful = require('contentful-management');
 
+/** Retrieves the `contentful-management` client.
+ * This is only used for codegen
+ * (use `utils/contentful.ts` for the content delivery API client) */
 module.exports = function () {
   const contentfulClient = contentful.createClient({
     accessToken: process.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
