1f356352ca7cd31e00f380fb4e65c4fba144ff0e
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-20 14:29:49 -0500

    Add types for page items and page item fields

--- @types/contentTypes.ts
+++ @types/contentTypes.ts
@@ -1,10 +1,11 @@
-/*
-contentful-typescript-codegen can't generate an enum of content types
-so we do it manually here
+import { Entry } from "contentful";
+import { IContentSection, IPage, IPageFields } from "./generated/contentful";
 
-https://github.com/intercom/contentful-typescript-codegen/issues/54
-*/
-
+/**
+ * `contentful-typescript-codegen` doesn't generate an enum of content types
+ * so we do it manually here. Github issue at:
+ * https://github.com/intercom/contentful-typescript-codegen/issues/54
+ */
 export enum ContentTypes {
     Announcement = 'announcement',
     AnnouncementCollection = 'announcementCollection',
@@ -21,3 +22,16 @@ export enum ContentTypes {
     Resource = 'resource',
     ResourceCollection = 'resourceCollection',
 };
+
+/** Get the fields from an array-like entry */
+type IEntryFieldsItem<T extends Entry<IPageFields | IPageFieldsItem['fields']>> = T['fields']['content'][number];
+
+/** Top-level collections on the page (announcement collection, event calendar, etc). `IContentSection` is excluded since it isn't a collection. */
+export type IPageFieldsItem = Exclude<IEntryFieldsItem<IPage>, IContentSection>;
+
+/** Child items on the page (content section, announcement, etc).*/
+export type IPageItemFieldsItem = IEntryFieldsItem<IPageFieldsItem> | IContentSection;
+
+export const isIPage = (block: IPage | IPageFieldsItem | IPageItemFieldsItem): block is IPage => (block as IPage).sys.contentType.sys.id === 'page';
+
+export const isIPageFieldsItem = (block: IPageFieldsItem | IPageItemFieldsItem): block is IPageFieldsItem => Array.isArray((block as IPageFieldsItem).fields.content);
