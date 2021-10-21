7c5e88527c7914532b36bdc1ff7f2e91eabec729
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-21 09:22:45 -0500

    Add route to render all internal resources

---
+++ pages/resources/[slug].tsx
@@ -0,0 +1,30 @@
+import { EntryCollection } from "contentful";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Params } from "next/dist/server/router";
+import { ContentTypes } from "../../@types/contentTypes";
+import { IResourceFields } from "../../@types/generated/contentful";
+import Resource from "../../components/Resource";
+import collectionData from '../../utils/collections.preval';
+
+export default Resource;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const resources = collectionData[ContentTypes.Resource] as EntryCollection<IResourceFields>;
+
+  const paths = resources.items.map((resource) => ({
+    params: { slug: resource.fields.slug }
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params: { slug = [] } }: Params) => {
+  const resources = collectionData[ContentTypes.Resource] as EntryCollection<IResourceFields>;
+  const resource = resources.items.find((resource) => resource.fields.slug === slug);
+
+  return {
+    props: {
+      entry: resource,
+    },
+  };
+}
