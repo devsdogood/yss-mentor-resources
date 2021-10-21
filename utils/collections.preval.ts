9b0f3fd143c15ab6ea1dd7aefc6299b2e63d69c5
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-21 08:45:56 -0500

    Add function to preval collection entries

---
+++ utils/collections.preval.ts
@@ -0,0 +1,23 @@
+import preval from 'next-plugin-preval';
+import { ContentTypes } from '../@types/contentTypes';
+import getContentful from './contentful';
+
+const collections = [
+    ContentTypes.Announcement,
+    ContentTypes.Event,
+    ContentTypes.Facilitator,
+    ContentTypes.Newsletter,
+    ContentTypes.Resource
+];
+
+const getCollections = async () => {
+    const contentful = await getContentful();
+    const collectionMap = await Promise.all(collections.map(async (collection) => [
+        collection,
+        await contentful.getEntries({ content_type: collection, include: 2 }),
+    ]));
+
+    return Object.fromEntries(collectionMap);
+}
+
+export default preval(getCollections());
