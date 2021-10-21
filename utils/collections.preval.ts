377c4c6f70efd270a0398a7fcbc198a57432c147
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-21 09:57:14 -0500

    Order collection by created descending

--- utils/collections.preval.ts
+++ utils/collections.preval.ts
@@ -15,7 +15,7 @@ const getCollections = async () => {
     const contentful = await getContentful();
     const collectionMap = await Promise.all(collections.map(async (collection) => [
         collection,
-        await contentful.getEntries({ content_type: collection, include: 2 }),
+        await contentful.getEntries({ content_type: collection, include: 2, order: '-sys.createdAt' }),
     ]));
 
     return Object.fromEntries(collectionMap);
