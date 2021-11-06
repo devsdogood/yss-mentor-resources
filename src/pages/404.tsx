e9434ab93132de495762fe54256dee2116a677e9
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2021-11-06 18:08:40 -0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Custom404Page = () => (
   <div className="columns is-centered is-mobile">
     <div className="column is-half">
-      <Image layout="responsive" width={100} height={100} src="/rick-roll.gif"></Image>
+      <Image layout="responsive" width={273} height={350} src="/rick-roll.gif"></Image>
       <h1 className="title is-1">Oopsie! Looks like the website broke.</h1>
       <h2 className="title is-6">
         We promise that some developer somewhere is going 🍌 right now!
