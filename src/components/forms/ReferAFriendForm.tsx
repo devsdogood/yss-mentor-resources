107a21b7edd0a0c8f81a5e73830082a7ba351fe5
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-24 19:39:22 -0600

    Add ability to submit the referral form

--- src/components/forms/ReferAFriendForm.tsx
+++ src/components/forms/ReferAFriendForm.tsx
@@ -1,21 +1,46 @@
 import { FormikProvider, useFormik } from "formik";
+import { useState } from "react";
 import { AnySchema } from "yup";
 import TextField from "./fields/TextField";
 import { referAFriendSchema } from "./schemas/ReferAFriendSchema";
 
 const ReferAFriendForm = () => {
+    const [hasSubmitted, setHasSubmitted] = useState(false);
     const fields = Object.entries(referAFriendSchema.fields) as [string, AnySchema][];
 
     const formik = useFormik({
-        initialValues: {},
+        initialValues: Object.fromEntries(fields.map(([key]) => [key, ''])),
         validationSchema: referAFriendSchema,
-        onSubmit: (values) => {
-            console.log(values);
+        onSubmit: async (values, { resetForm }) => {
+            await fetch(
+                `https://formsubmit.co/ajax/${process.env.NEXT_PUBLIC_FORM_EMAIL}`,
+                {
+                  method: "POST",
+                  headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(values),
+                }
+            );
+
+            resetForm();
+            setHasSubmitted(true);
         },
     });
 
+    const FormSuccessMessage = (
+        <article className="message is-primary">
+            <div className="message-body">
+                Form successfully submitted. Thank you for referring your friend!
+            </div>
+        </article>
+    );
+
     return (
         <FormikProvider value={formik}>
+            {hasSubmitted && FormSuccessMessage}
+
             <div className="columns is-multiline">
                 {fields.map(([name, field]) => (
                     <div
@@ -26,7 +51,11 @@ const ReferAFriendForm = () => {
                     </div>
                 ))}
             </div>
-            <button className="button is-primary" onClick={formik.submitForm}>
+
+            <button
+                className={`button is-primary ${formik.isSubmitting && 'is-loading'}`}
+                onClick={formik.submitForm}
+            >
                 Submit
             </button>
         </FormikProvider>
